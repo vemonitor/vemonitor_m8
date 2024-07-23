20b87e75802088f3078866cef483d1bf6d288267
zepi <eli.serra173@gmail.com>
2024-07-23 12:53:29 +0200

    add AppBlockRun unit test methods

---
+++ test/test_async_app_run.py
@@ -0,0 +1,52 @@
+"""Test AppBlockRun method."""
+
+import inspect
+from os import path as Opath
+import pytest
+from vemonitor_m8.conf_manager.config_loader import ConfigLoader
+from vemonitor_m8.core.async_app_run import AppBlockRun
+
+
+@pytest.fixture(name="helper_manager", scope="class")
+def helper_manager_fixture():
+    """Json Schema test manager fixture"""
+    class HelperManager:
+        """Json Helper test manager fixture Class"""
+        def __init__(self):
+            current_script_path = Opath.dirname(
+            Opath.abspath(
+                    inspect.getfile(inspect.currentframe())
+                )
+            )
+            self.test_path = Opath.join(
+                current_script_path,
+                "conf"
+            )
+            self.loader = ConfigLoader(self.test_path)
+
+    return HelperManager()
+
+
+class TestAppBlockRun:
+    """Test AppBlockRun method."""
+
+    def test_run_block(self, helper_manager):
+        """Test subscribe_worker_data_ready method."""
+        conf=helper_manager.loader.get_settings_from_schema(
+            block_name=None,
+            app_name="batSerialMonitor",
+        )
+        obj = AppBlockRun(
+            conf=conf
+        )
+
+        assert obj.is_ready()
+
+         # inputs workers blocks run on background (Threads), executed by a timer
+        obj.add_input_items_timer()
+        # inputs workers blocks run
+        obj.setup_outputs_workers()
+
+        assert obj.workers.has_input_workers()
+        assert obj.workers.has_output_workers()
+        obj.cancel_all_timers()
