ccbb74c22eed6b794b266ac41f039d78ab9634ff
zepi <eli.serra173@gmail.com>
2024-07-26 15:08:39 +0200

    Add is_valid_port and is_valid_host methods

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 """Test Utils class."""
-from vemonitor_m8.core.utils import Utils as Utvm
+from vemonitor_m8.core.utils import Utils as Ut
 
 class TestUtils:
     """Test Utils class."""
 
     def test_get_min_in_loop(self):
         """Test check_columns method."""
-        result = Utvm.get_min_in_loop(
+        result = Ut.get_min_in_loop(
             value=0,
             min_val=6
         )
         assert result == 6
 
-        result = Utvm.get_min_in_loop(
+        result = Ut.get_min_in_loop(
             value=12,
             min_val=6
         )
         assert result == 6
 
-        result = Utvm.get_min_in_loop(
+        result = Ut.get_min_in_loop(
             value=3,
             min_val=6
         )
@@ -28,19 +28,19 @@ class TestUtils:
 
     def test_get_max_in_loop(self):
         """Test get_max_in_loop method."""
-        result = Utvm.get_max_in_loop(
+        result = Ut.get_max_in_loop(
             value=0,
             max_val=6
         )
         assert result == 6
 
-        result = Utvm.get_max_in_loop(
+        result = Ut.get_max_in_loop(
             value=3,
             max_val=6
         )
         assert result == 6
 
-        result = Utvm.get_max_in_loop(
+        result = Ut.get_max_in_loop(
             value=12,
             max_val=6
         )
@@ -48,27 +48,56 @@ class TestUtils:
 
     def test_all_equal(self):
         """Test all_equal method."""
-        result = Utvm.all_equal(
+        result = Ut.all_equal(
             iterable=[1, 1, 1, 1, 1]
         )
         assert result is True
 
-        result = Utvm.all_equal(
+        result = Ut.all_equal(
             iterable=[1, 1, 2, 1, 1]
         )
         assert result is False
 
-        result = Utvm.all_equal(
+        result = Ut.all_equal(
             iterable=(1, 1, 1, 1, 1)
         )
         assert result is True
 
-        result = Utvm.all_equal(
+        result = Ut.all_equal(
             iterable=(1, 1, 2, 1, 1)
         )
         assert result is False
 
-        result = Utvm.all_equal(
+        result = Ut.all_equal(
             iterable={'a': 1, 'b': 1, 'c': 1, 'd': 1}
         )
         assert result is False
+    
+    def test_is_valid_port(self):
+        """Test is_valid_port method."""
+        assert Ut.is_valid_port(0) is False
+        assert Ut.is_valid_port(-1) is False
+        assert Ut.is_valid_port(65536) is False
+        assert Ut.is_valid_port(1) is True
+        assert Ut.is_valid_port(65535) is True
+
+    def test_is_valid_host(self):
+        """Test is_valid_host method."""
+        assert Ut.is_valid_host(0) is False
+        assert Ut.is_valid_host('0') is False
+        assert Ut.is_valid_host('0.0') is False
+        assert Ut.is_valid_host('0.0.0') is False
+        assert Ut.is_valid_host('256.255.255.255') is False
+        assert Ut.is_valid_host('255.256.255.255') is False
+        assert Ut.is_valid_host('255.255.256.255') is False
+        assert Ut.is_valid_host('255.255.255.256') is False
+        assert Ut.is_valid_host('-1.255.255.255') is False
+        assert Ut.is_valid_host('255.-1.255.255') is False
+        assert Ut.is_valid_host('255.255.-1.255') is False
+        assert Ut.is_valid_host('255.255.255.-1') is False
+
+
+        assert Ut.is_valid_host('0.0.0.0') is True
+        assert Ut.is_valid_host('255.255.255.255') is True
+        assert Ut.is_valid_host('127.0.0.1') is True
+        assert Ut.is_valid_host('192.168.1.1') is True
